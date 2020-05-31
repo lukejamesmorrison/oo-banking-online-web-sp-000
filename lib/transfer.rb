f1556d7e9be2531706d4cbff61c3e185abbfc8ca
Luke Morrison <lukejamesmorrison@gmail.com>
2020-05-31 13:30:52 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,12 @@
 class Transfer
 
-  def initialize()
-    
+  attr_reader :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = 'pending'
+    @amount = amount
   end
 
 end
