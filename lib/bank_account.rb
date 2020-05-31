870cf55908098606119b89be0cd8d54b0b105b88
Luke Morrison <lukejamesmorrison@gmail.com>
2020-05-31 13:31:09 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,11 +1,14 @@
 class BankAccount
 
   attr_reader :name, :balance, :status
-  
+
   def initialize(name)
     @name = name
     @balance = 1000
     @status = 'open'
+  end
+
+  def deposit(amount)
   end
 
 end
